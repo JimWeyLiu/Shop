d8f6addb71dee781e0a279c25f8b13c8f6c47fea
JimWeyLiu <amer22662747@gmail.com>
2021-08-28 21:17:19 +0800

    can set email & password

--- app/src/main/java/com/jim/shop/MainActivity.kt
+++ app/src/main/java/com/jim/shop/MainActivity.kt
@@ -34,17 +34,17 @@ class MainActivity : AppCompatActivity() {
                 .setAction("Action", null).show()
         }
     }
+    //=====================================================↓
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) { //覆寫onActivityResult
         super.onActivityResult(requestCode, resultCode, data)
 
     }
-    //=====================================================↓
+    //=====================================================↑
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
-    //=====================================================↑
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
--- app/src/main/java/com/jim/shop/SignUpActivity.kt
+++ app/src/main/java/com/jim/shop/SignUpActivity.kt
@@ -2,10 +2,21 @@ package com.jim.shop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        signup.setOnClickListener {
+            val sEmail:String = email.text.toString()
+            val sPassword:String = password.text.toString()
+            FirebaseAuth
+                .getInstance()//取得Firebase的物件
+                .createUserWithEmailAndPassword(sEmail,sPassword)// 創建帳密(mail,password)
+            Log.d("onCreate: ",sEmail+sPassword)
+        }
     }
 }
